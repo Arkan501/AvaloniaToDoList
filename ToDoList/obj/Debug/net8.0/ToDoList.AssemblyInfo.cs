e03e1c739fa2b418bea01b2c8fff4b5f9b661737
Arkan <arkan501@proton.me>
2024-02-21 00:19:19 -0700

    Push

--- ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9068140eae98ac71fc1bab8caed1dc3b98f98b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2993fd82eddfa29d16993544a5d1d77bca5117b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
